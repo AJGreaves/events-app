b0db35cbef15a5e7339099e98faa5261dfa7ed3d
AJGreaves <>
2023-10-02 14:11:45 +0000

    Update event_detail view for simpler database request

--- events/views.py
+++ events/views.py
@@ -6,15 +6,6 @@ from .models import Event, Ticket
 
 
 class EventsList(generic.ListView):
-    """
-    This is Django's generic ListView. We are a little limited as to
-    what we can do with this, but if we really needed to, we could
-    access the request object here too.
-
-    It's a class, which is why we had used class-based views in the
-    original blog material. Now, to show what's going on under the
-    hood, so to speak, we'll use function-based ones instead.
-    """
 
     model = Event
     template_name = "index.html"
@@ -23,10 +14,11 @@ class EventsList(generic.ListView):
 
 def event_detail(request, event_id, *args, **kwargs):
 
-    event = get_object_or_404(Event, pk=event_id)
+    queryset = Event.objects.filter()
+    event = get_object_or_404(queryset, pk=event_id)
 
     return render(
         request,
-        "events/event_ticket_list.html",
+        "events/event_detail.html",
         {"event": event}
     )
